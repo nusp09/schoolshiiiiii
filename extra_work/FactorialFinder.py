11a3086e24fd41f568151d4f83784adfcaf1b10f
oscar o <21overallo@kenstimpson.org.uk>
2025-02-03 12:47:05 +0000

    Update FactorialFinder.py

--- extra_work/FactorialFinder.py
+++ extra_work/FactorialFinder.py
@@ -1,1 +1,12 @@
-
+while True:
+    try:
+        num = int(input('enter a number'))
+        break
+    except:
+        print('thats not a number')
+fact = num
+total = num
+for i in range(num - 1):
+    fact -= 1
+    total *= fact
+print(f'the factorial is {total}')
